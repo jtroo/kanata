a9d09d08eef55252e7e1a5a292c4335676bf759e
Itay Garin <thifixp@gmail.com>
2020-05-23 18:07:18 +0300

    Add sticky.rs

---
+++ src/effects/sticky.rs
@@ -0,0 +1,31 @@
+use std::collections::HashSet;
+use crate::keys::KeyCode;
+use log::debug;
+
+pub struct StickyState {
+    pressed: HashSet<KeyCode>
+}
+
+impl StickyState {
+    pub fn new() -> Self {
+        Self{pressed: HashSet::new()}
+    }
+
+    pub fn update_pressed(&mut self, key: KeyCode) {
+        debug!("Activating sticky {:?}", key);
+        self.pressed.insert(key);
+    }
+
+    pub fn update_released(&mut self, key: KeyCode) {
+        debug!("Deactivating sticky {:?}", key);
+        self.pressed.remove(&key);
+    }
+
+    pub fn is_pressed(&mut self, key: KeyCode) -> bool {
+        self.pressed.contains(&key)
+    }
+
+    pub fn is_idle(&self) -> bool {
+        self.pressed.len() == 0
+    }
+}
