dc913e69b053a82943702e8029662c10251f1284
Itay Garin <thifixp@gmail.com>
2020-06-01 21:35:33 +0300

    Add a sample python client as well

---
+++ py/ktrl_client.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import zmq
+
+context = zmq.Context()
+
+#  Socket to talk to server
+print("Connecting to hello world server…")
+socket = context.socket(zmq.REQ)
+socket.connect("tcp://127.0.0.1:7331")
+
+#  Do 10 requests, waiting each time for a response
+for request in range(10):
+    print("Sending request %s …" % request)
+    socket.send(b"Hello")
+
+    #  Get the reply.
+    message = socket.recv()
+    print("Received reply %s [ %s ]" % (request, message))
