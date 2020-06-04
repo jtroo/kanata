36120d6f46abb3b8cd155794f8b629e5c63020ee
Itay Garin <thifixp@gmail.com>
2020-06-04 15:40:20 +0300

    Improve the python ipc client cli

--- py/ktrl_client.py
+++ py/ktrl_client.py
@@ -1,26 +1,38 @@
 #!/usr/bin/python3
 import zmq
-import sys
+import argparse
+
+DEFAULT_IPC_PORT = 7331
+
+#
+# Example Usage:
+# --------------
+#
+# ./ktrl_client.py --port 123456 "IpcDoEffect((fx: NoOp, val: Press))"
+# ./ktrl_client.py "IpcDoEffect((fx: NoOp, val: Press))"
+#
 
 def main():
-    if len(sys.argv) == 0:
-        port = 7331
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--port", help="ktrl's ipc port")
+    parser.add_argument("msg", help="ipc msg to send to ktrl")
+    args = parser.parse_args()
+
+    if args.port == None:
+        port = DEFAULT_IPC_PORT
     else:
-        port = sys.argv[1]
+        port = int(args.port)
 
     context = zmq.Context()
 
-    #  Socket to talk to server
     endpoint = "tcp://127.0.0.1:" + str(port)
     print("Connecting to ktrl's ipc server: " + endpoint)
     socket = context.socket(zmq.REQ)
     socket.connect(endpoint)
 
-    req = b"IpcDoEffect((fx: NoOp, val: Press))"
-    print("Sending request %s" % req)
-    socket.send(req)
+    print("Sending request %s" % args.msg)
+    socket.send_string(args.msg)
 
-    #  Get the reply.
     message = socket.recv()
     print("Received reply [ %s ]" % message)
 
