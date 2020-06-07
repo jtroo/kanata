681407f340165c2362e3d7733d4f301cb1323573
Itay Garin <thifixp@gmail.com>
2020-06-07 21:59:04 +0300

    Fix trackpad issue with my Lenovo laptop

--- src/kbd_in.rs
+++ src/kbd_in.rs
@@ -16,7 +16,14 @@ impl KbdIn {
     pub fn new(dev_path: &Path) -> Result<Self, std::io::Error> {
         let kbd_in_file = File::open(dev_path)?;
         let mut kbd_in_dev = Device::new_from_fd(kbd_in_file)?;
+
+        // NOTE: This grab-ungrab-grab sequence magically
+        // fix an issue I had with my Lenovo Yoga trackpad not working.
+        // I honestly have no idea why this works haha.
         kbd_in_dev.grab(GrabMode::Grab)?;
+        kbd_in_dev.grab(GrabMode::Ungrab)?;
+        kbd_in_dev.grab(GrabMode::Grab)?;
+
         Ok(KbdIn { device: kbd_in_dev })
     }
 
