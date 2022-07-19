7572f1fe0a6ec7ecd1630bfc571c490b7cb77fed
Lin Jian <me@linj.tech>
2022-07-19 17:14:03 +0800

    Bump evdev-rs to 0.5.0

--- src/oskbd/linux.rs
+++ src/oskbd/linux.rs
@@ -48,7 +48,7 @@ impl KbdIn {
 
     fn new_linux(dev_path: &Path) -> Result<Self, std::io::Error> {
         let kbd_in_file = File::open(dev_path)?;
-        let mut kbd_in_dev = Device::new_from_fd(kbd_in_file)?;
+        let mut kbd_in_dev = Device::new_from_file(kbd_in_file)?;
 
         // NOTE: This grab-ungrab-grab sequence magically
         // fix an issue I had with my Lenovo Yoga trackpad not working.
