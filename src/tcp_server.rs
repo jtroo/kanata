87da166ed16e16787b38eef1f1a1ec22467be520
LGUG2Z <jadeiqbal@fastmail.com>
2022-07-21 07:31:05 -0700

    feat(tcp): add keep-alive, split client/server messages
    This commit sets a 30-second keep-alive on opened TcpStreams (using
    net2::TcpStreamExt as keep-alive functionality is not currently in the
    standard library) and splits the previous EventNotification enum into a
    ServerMessage and a ClientMessage enum respectively.
    
    I have changed the semantics between ServerMessage and ClientMessage
    slightly so that ClientMessage variants are imperative requests that
    align closer with the names of the handler functions, ie. ChangeLayer
    calls fn change_layer().
    
    Whenever a client's TcpStream cannot be written to, either because it
    has notified the server of a disconnect or because it has failed the
    keep-alive, it will be removed from the connections HashMap on the
    TcpServer struct.
    
    re #47

--- src/tcp_server.rs
+++ src/tcp_server.rs
@@ -1,25 +1,32 @@
 use crate::Kanata;
 use anyhow::Result;
+use net2::TcpStreamExt;
 use parking_lot::Mutex;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
-use std::io::{BufReader, Read};
+use std::io::Read;
 use std::net::{TcpListener, TcpStream};
 use std::str::FromStr;
 use std::sync::Arc;
+use std::time::Duration;
 
 #[derive(Debug, Serialize, Deserialize)]
-pub enum EventNotification {
+pub enum ServerMessage {
     LayerChange { new: String },
 }
 
-impl EventNotification {
+#[derive(Debug, Serialize, Deserialize)]
+pub enum ClientMessage {
+    ChangeLayer { new: String },
+}
+
+impl ServerMessage {
     pub fn as_bytes(&self) -> Result<Vec<u8>> {
         Ok(serde_json::to_string(self)?.as_bytes().to_vec())
     }
 }
 
-impl FromStr for EventNotification {
+impl FromStr for ClientMessage {
     type Err = serde_json::Error;
 
     fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
@@ -44,22 +51,27 @@ impl TcpServer {
         let listener = TcpListener::bind(format!("0.0.0.0:{}", self.port))
             .expect("could not start the tcp server");
 
-        let cl = self.connections.clone();
+        let connections = self.connections.clone();
+
         std::thread::spawn(move || {
             for stream in listener.incoming() {
                 match stream {
                     Ok(stream) => {
+                        stream
+                            .set_keepalive(Some(Duration::from_secs(30)))
+                            .expect("could not set tcp connection keepalive");
+
                         let addr = stream
                             .peer_addr()
                             .expect("could not find peer address")
                             .to_string();
 
                         {
-                            cl.lock().insert(addr.clone(), stream);
+                            connections.lock().insert(addr.clone(), stream);
                         }
 
-                        if let Some(stream) = cl.lock().get(&addr) {
-                            let stream = stream
+                        if let Some(stream) = connections.lock().get(&addr) {
+                            let mut stream = stream
                                 .try_clone()
                                 .expect("could not clone tcpstream to read incoming messages");
 
@@ -67,14 +79,13 @@ impl TcpServer {
                             std::thread::spawn(move || {
                                 log::info!("listening for incoming messages {}", &addr);
                                 loop {
-                                    let mut buffer: [u8; 1024] = [0; 1024];
-                                    let mut reader = BufReader::new(&stream);
-                                    if let Ok(size) = reader.read(&mut buffer) {
-                                        if let Ok(event) = EventNotification::from_str(
-                                            &String::from_utf8_lossy(&buffer[..size]),
+                                    let mut buf = vec![0; 1024];
+                                    if let Ok(size) = stream.read(&mut buf) {
+                                        if let Ok(event) = ClientMessage::from_str(
+                                            &String::from_utf8_lossy(&buf[..size]),
                                         ) {
                                             match event {
-                                                EventNotification::LayerChange { new } => {
+                                                ClientMessage::ChangeLayer { new } => {
                                                     k_cl.lock().change_layer(new);
                                                 }
                                             }
