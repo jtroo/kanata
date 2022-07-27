7dc41eb1630ad4769e4b8a541b21406a38f24cd0
LGUG2Z <jadeiqbal@fastmail.com>
2022-07-27 09:22:16 -0700

    feat(keycodes): add mute

--- src/keys/mod.rs
+++ src/keys/mod.rs
@@ -110,6 +110,7 @@ pub fn str_to_oscode(s: &str) -> Option<OsCode> {
         "home" => OsCode::KEY_HOME,
         "end" => OsCode::KEY_END,
         "nlck" | "nlk" => OsCode::KEY_NUMLOCK,
+        "mute" => OsCode::KEY_MUTE,
         "volu" => OsCode::KEY_VOLUMEUP,
         "voldwn" | "vold" => OsCode::KEY_VOLUMEDOWN,
         "brup" | "bru" => OsCode::KEY_BRIGHTNESSUP,
