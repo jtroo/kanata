83142099f975e1a87d96648c9f8b6e7cfbfcaa76
Jan Tache <j.andreitabs@gmail.com>
2022-12-07 00:26:07 -0800

    feat(win-interception): implement mouse btns in defsrc
    This commit implements mouse buttons in defsrc for Windows with the use
    of the interception driver. It includes a new defcfg item which states
    which mouse hardware ID to intercept. Since the intended use case is for
    laptops that have builtin mouse buttons like Lenovo Thinkpads, only a
    single hardware ID is allowed for now.
    
    This commit also fixes a bug in the parsing which probably only affected
    the `cmd` action. The bug is that quoted strings keep their quotes when
    parsed as an atom by the S-expression parser. This was introduced in
    v1.0.8. This behaviour is not changed; instead, the quotes are trimmed
    when parsing `cmd` and `defcfg`. If one day the parser is properly fixed
    to remove the leading and trailing quotes from atoms, then the trimming
    code should still be fine; `trim_matches` will be fine with or without
    the quotes.

--- src/kanata/windows/interception.rs
+++ src/kanata/windows/interception.rs
@@ -8,6 +8,7 @@ use crate::kanata::*;
 use crate::keys::{KeyValue, OsCode};
 
 static PRESSED_KEYS: Lazy<Mutex<HashSet<OsCode>>> = Lazy::new(|| Mutex::new(HashSet::default()));
+const HWID_ARR_SZ: usize = 128;
 
 impl Kanata {
     pub fn event_loop(kanata: Arc<Mutex<Self>>, tx: Sender<KeyEvent>) -> Result<()> {
@@ -20,13 +21,28 @@ impl Kanata {
             state: ic::KeyState::empty(),
             information: 0,
         }; 32];
+        intrcptn.set_filter(ic::is_mouse, ic::Filter::MouseFilter(ic::MouseState::all()));
+
+        let mouse_to_intercept_hwid: Option<[u8; HWID_ARR_SZ]> = kanata
+            .lock()
+            .intercept_mouse_hwid.as_ref()
+            .map(|hwid| {
+                hwid.iter().copied().enumerate()
+                    .fold([0u8; HWID_ARR_SZ], |mut hwid, idx_byte| {
+                        let (i, b) = idx_byte;
+                        if i > HWID_ARR_SZ {
+                            panic!("windows-interception-mouse-hwid is too long; it should be up to {HWID_ARR_SZ} 8-bit unsigned integers");
+                        }
+                        hwid[i] = b;
+                        hwid
+                    })
+            });
+        let mut is_dev_interceptable: HashMap<ic::Device, bool> = HashMap::default();
 
         loop {
             let dev = intrcptn.wait_with_timeout(std::time::Duration::from_millis(1));
             if dev > 0 {
-                let num_strokes = intrcptn.receive(dev, &mut strokes);
-                let num_strokes = num_strokes as usize;
-
+                let num_strokes = intrcptn.receive(dev, &mut strokes) as usize;
                 for i in 0..num_strokes {
                     log::debug!("got stroke {:?}", strokes[i]);
                     let mut key_event = match strokes[i] {
@@ -45,9 +61,26 @@ impl Kanata {
                             };
                             KeyEvent { code, value }
                         }
-                        _ => {
-                            intrcptn.send(dev, &strokes[i..i + 1]);
-                            continue;
+                        ic::Stroke::Mouse { state, .. } => {
+                            log::trace!("matched on mouse stroke");
+                            if let Some(hwid) = mouse_to_intercept_hwid {
+                                log::trace!("checking mouse state to event");
+                                if let Some(event) = mouse_state_to_event(
+                                    dev,
+                                    &hwid,
+                                    state,
+                                    &intrcptn,
+                                    &mut is_dev_interceptable,
+                                ) {
+                                    event
+                                } else {
+                                    intrcptn.send(dev, &strokes[i..i + 1]);
+                                    continue;
+                                }
+                            } else {
+                                intrcptn.send(dev, &strokes[i..i + 1]);
+                                continue;
+                            }
                         }
                     };
                     check_for_exit(&key_event);
@@ -99,5 +132,82 @@ impl Kanata {
                 Err(TryRecvError::Empty) => {}
             }
         }
+    }
+}
+
+fn mouse_state_to_event(
+    input_dev: ic::Device,
+    allowed_hwid: &[u8; HWID_ARR_SZ],
+    state: ic::MouseState,
+    intrcptn: &ic::Interception,
+    is_dev_interceptable: &mut HashMap<ic::Device, bool>,
+) -> Option<KeyEvent> {
+    if !match is_dev_interceptable.get(&input_dev) {
+        Some(v) => *v,
+        None => {
+            let mut hwid = [0u8; HWID_ARR_SZ];
+            log::trace!("getting hardware id for input dev: {input_dev}");
+            let res = intrcptn.get_hardware_id(input_dev, &mut hwid);
+            let dev_is_interceptable = hwid == *allowed_hwid;
+            log::info!("res {res}; device #{input_dev} hwid {hwid:?} matches allowed mouse input: {dev_is_interceptable}");
+            is_dev_interceptable.insert(input_dev, dev_is_interceptable);
+            dev_is_interceptable
+        }
+    } {
+        return None;
+    }
+
+    if state.contains(ic::MouseState::RIGHT_BUTTON_DOWN) {
+        Some(KeyEvent {
+            code: OsCode::BTN_RIGHT,
+            value: KeyValue::Press,
+        })
+    } else if state.contains(ic::MouseState::RIGHT_BUTTON_UP) {
+        Some(KeyEvent {
+            code: OsCode::BTN_RIGHT,
+            value: KeyValue::Release,
+        })
+    } else if state.contains(ic::MouseState::LEFT_BUTTON_DOWN) {
+        Some(KeyEvent {
+            code: OsCode::BTN_LEFT,
+            value: KeyValue::Press,
+        })
+    } else if state.contains(ic::MouseState::LEFT_BUTTON_UP) {
+        Some(KeyEvent {
+            code: OsCode::BTN_LEFT,
+            value: KeyValue::Release,
+        })
+    } else if state.contains(ic::MouseState::MIDDLE_BUTTON_DOWN) {
+        Some(KeyEvent {
+            code: OsCode::BTN_MIDDLE,
+            value: KeyValue::Press,
+        })
+    } else if state.contains(ic::MouseState::MIDDLE_BUTTON_UP) {
+        Some(KeyEvent {
+            code: OsCode::BTN_MIDDLE,
+            value: KeyValue::Release,
+        })
+    } else if state.contains(ic::MouseState::BUTTON_4_DOWN) {
+        Some(KeyEvent {
+            code: OsCode::BTN_SIDE,
+            value: KeyValue::Press,
+        })
+    } else if state.contains(ic::MouseState::BUTTON_4_UP) {
+        Some(KeyEvent {
+            code: OsCode::BTN_SIDE,
+            value: KeyValue::Release,
+        })
+    } else if state.contains(ic::MouseState::BUTTON_5_DOWN) {
+        Some(KeyEvent {
+            code: OsCode::BTN_EXTRA,
+            value: KeyValue::Press,
+        })
+    } else if state.contains(ic::MouseState::BUTTON_5_UP) {
+        Some(KeyEvent {
+            code: OsCode::BTN_EXTRA,
+            value: KeyValue::Release,
+        })
+    } else {
+        None
     }
 }
