jtroo/kanata

commit 55e952dacab67138c402554b11ec57fbaf5205ab

dep: cleanup, panic = "abort", update clap
This commit cleans up dependencies and dependency features with the goal
of reducing compile times and code size. In the same goal, the line:

    panic = "abort"

is added to Cargo.toml. Currently kanata does not rely on unwinding
anywhere, so this seems fine to do.

The clap crate is updated to the latest, for no particular reason other
than keeping it up to date.

Cargo.toml
@@ -12,16 +12,15 @@ license = "LGPL-3.0"
 edition = "2021"
 
 [dependencies]
-clap = { version = "3", features = [ "derive" ] }
-log = "0.4.8"
+clap = { version = "4.1", features = [ "std", "derive" ], default_features = false }
+log = { version = "0.4.8", default_features = false }
 simplelog = "0.12.0"
 anyhow = "1"
 parking_lot = "0.12"
 crossbeam-channel = "0.5"
 once_cell = "1"
-serde = { version = "1", features = ["derive"] }
-serde_json = "1"
-net2 = "0.2"
+serde = { version = "1", features = ["alloc", "derive"], default_features = false }
+serde_json = { version = "1", features = ["alloc"], default_features = false }
 radix_trie = "0.2"
 rustc-hash = "1.1.0"
 
@@ -46,7 +45,7 @@ winapi = { version = "0.3.9", features = [
     "timeapi",
     "mmsystem",
 ] }
-native-windows-gui = "1.0.12"
+native-windows-gui = { version = "1.0.12", default_features = false }
 interception = { version = "0.1.2", optional = true }
 
 [features]
@@ -57,3 +56,4 @@ interception_driver = ["interception"]
 [profile.release]
 opt-level = "z"
 lto = "fat"
+panic = "abort"

src/tcp_server.rs
@@ -1,12 +1,10 @@
 use crate::Kanata;
-use net2::TcpStreamExt;
 use parking_lot::Mutex;
 use serde::{Deserialize, Serialize};
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
 use std::str::FromStr;
 use std::sync::Arc;
-use std::time::Duration;
 
 type HashMap<K, V> = rustc_hash::FxHashMap<K, V>;
 
@@ -68,10 +66,6 @@ impl TcpServer {
             for stream in listener.incoming() {
                 match stream {
                     Ok(mut stream) => {
-                        stream
-                            .set_keepalive(Some(Duration::from_secs(30)))
-                            .expect("TCP keepalive is set");
-
                         {
                             let k = kanata.lock();
                             log::info!(

commit 4e46596277954f94fd0052bc4a7c581e23309993

fix(cli): add clap features
This commit fixes the help flags in particular which were broken by
commit 55e952d. It also re-enables suggestions which suggests the help
flags if an error is encountered.

Cargo.toml
@@ -12,7 +12,7 @@ license = "LGPL-3.0"
 edition = "2021"
 
 [dependencies]
-clap = { version = "4.1", features = [ "std", "derive" ], default_features = false }
+clap = { version = "4.1.6", features = [ "std", "derive", "help", "suggestions" ], default_features = false }
 log = { version = "0.4.8", default_features = false }
 simplelog = "0.12.0"
 anyhow = "1"

src/main.rs
@@ -25,37 +25,42 @@ pub struct ValidatedArgs {
 }
 
 #[derive(Parser, Debug)]
-#[clap(author, version, about, verbatim_doc_comment)]
+#[command(author, version, verbatim_doc_comment)]
 /// kanata: an advanced software key remapper
 ///
 /// kanata remaps key presses to other keys or complex actions depending on the
 /// configuration for that key. You can find the guide for creating a config
-/// file here:    https://github.com/jtroo/kanata/blob/main/docs/config.adoc
+/// file here:
+///
+///     https://github.com/jtroo/kanata/blob/main/docs/config.adoc
 ///
 /// If you need help, please feel welcome to create an issue or discussion in
-/// the kanata repository:     https://github.com/jtroo/kanata
+/// the kanata repository:
+///
+///     https://github.com/jtroo/kanata
 struct Args {
-    /// Configuration file(s) to use with kanata. If not specified, defaults to kanata.kbd in the
-    /// current working directory.
-    #[clap(short, long, default_value = "kanata.kbd")]
+    /// Configuration file(s) to use with kanata. If not specified, defaults to
+    /// kanata.kbd in the current working directory.
+    #[arg(short, long, default_value = "kanata.kbd", verbatim_doc_comment)]
     cfg: Vec<String>,
 
-    /// Port to run the optional TCP server on. If blank, no TCP port will be listened on.
-    #[clap(short, long)]
+    /// Port to run the optional TCP server on. If blank, no TCP port will be
+    /// listened on.
+    #[arg(short, long, verbatim_doc_comment)]
     port: Option<i32>,
 
-    /// Path for the symlink pointing to the newly-created device. If blank, no symlink will be
-    /// created.
+    /// Path for the symlink pointing to the newly-created device. If blank, no
+    /// symlink will be created.
     #[cfg(target_os = "linux")]
-    #[clap(short, long)]
+    #[arg(short, long, verbatim_doc_comment)]
     symlink_path: Option<String>,
 
     /// Enable debug logging.
-    #[clap(short, long)]
+    #[arg(short, long)]
     debug: bool,
 
     /// Enable trace logging; implies --debug as well.
-    #[clap(short, long)]
+    #[arg(short, long)]
     trace: bool,
 }
 