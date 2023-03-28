993a5bba3f139d066e917528b3eafffd719d1744
jtroo <j.andreitabs@gmail.com>
2023-03-27 23:18:26 -0700

    feat: add caps-word (#358)

--- src/cfg/mod.rs
+++ src/cfg/mod.rs
@@ -1013,6 +1013,8 @@ fn parse_action_list(ac: &[SExpr], s: &ParsedState) -> Result<&'static KanataAct
         "cmd" => parse_cmd(&ac[1..], s, CmdType::Standard),
         "cmd-output-keys" => parse_cmd(&ac[1..], s, CmdType::OutputKeys),
         "fork" => parse_fork(&ac[1..], s),
+        "caps-word" => parse_caps_word(&ac[1..], s),
+        "caps-word-custom" => parse_caps_word_custom(&ac[1..], s),
         _ => bail_expr!(&ac[0], "Unknown action type: {ac_type}"),
     }
 }
@@ -2244,6 +2246,103 @@ fn parse_fork(ac_params: &[SExpr], s: &ParsedState) -> Result<&'static KanataAct
         right,
         right_triggers,
     }))))
+}
+
+fn parse_caps_word(ac_params: &[SExpr], s: &ParsedState) -> Result<&'static KanataAction> {
+    const ERR_STR: &str = "caps-word expects 1 param: <timeout>";
+    if ac_params.len() != 1 {
+        bail!("{ERR_STR}\nFound {} params instead of 1", ac_params.len());
+    }
+    let timeout = parse_non_zero_u16(&ac_params[0], s, "timeout")?;
+    Ok(s.a.sref(Action::Custom(s.a.sref(s.a.sref_slice(
+        CustomAction::CapsWord(CapsWordCfg {
+            keys_to_capitalize: &[
+                KeyCode::A,
+                KeyCode::B,
+                KeyCode::C,
+                KeyCode::D,
+                KeyCode::E,
+                KeyCode::F,
+                KeyCode::G,
+                KeyCode::H,
+                KeyCode::I,
+                KeyCode::J,
+                KeyCode::K,
+                KeyCode::L,
+                KeyCode::M,
+                KeyCode::N,
+                KeyCode::O,
+                KeyCode::P,
+                KeyCode::Q,
+                KeyCode::R,
+                KeyCode::S,
+                KeyCode::T,
+                KeyCode::U,
+                KeyCode::V,
+                KeyCode::W,
+                KeyCode::X,
+                KeyCode::Y,
+                KeyCode::Z,
+                KeyCode::Minus,
+            ],
+            keys_nonterminal: &[
+                KeyCode::Kb0,
+                KeyCode::Kb1,
+                KeyCode::Kb2,
+                KeyCode::Kb3,
+                KeyCode::Kb4,
+                KeyCode::Kb5,
+                KeyCode::Kb6,
+                KeyCode::Kb7,
+                KeyCode::Kb8,
+                KeyCode::Kb9,
+                KeyCode::Kp0,
+                KeyCode::Kp1,
+                KeyCode::Kp2,
+                KeyCode::Kp3,
+                KeyCode::Kp4,
+                KeyCode::Kp5,
+                KeyCode::Kp6,
+                KeyCode::Kp7,
+                KeyCode::Kp8,
+                KeyCode::Kp9,
+                KeyCode::BSpace,
+                KeyCode::Delete,
+                KeyCode::Up,
+                KeyCode::Down,
+                KeyCode::Left,
+                KeyCode::Right,
+            ],
+            timeout,
+        }),
+    )))))
+}
+
+fn parse_caps_word_custom(ac_params: &[SExpr], s: &ParsedState) -> Result<&'static KanataAction> {
+    const ERR_STR: &str = "caps-word-custom expects 3 param: <timeout> <keys-to-capitalize> <extra-non-terminal-keys>";
+    if ac_params.len() != 3 {
+        bail!("{ERR_STR}\nFound {} params instead of 1", ac_params.len());
+    }
+    let timeout = parse_non_zero_u16(&ac_params[0], s, "timeout")?;
+    Ok(s.a.sref(Action::Custom(
+        s.a.sref(
+            s.a.sref_slice(CustomAction::CapsWord(CapsWordCfg {
+                keys_to_capitalize: s.a.sref_vec(
+                    parse_key_list(&ac_params[1], s, "keys-to-capitalize")?
+                        .into_iter()
+                        .map(KeyCode::from)
+                        .collect(),
+                ),
+                keys_nonterminal: s.a.sref_vec(
+                    parse_key_list(&ac_params[2], s, "extra-non-terminal-keys")?
+                        .into_iter()
+                        .map(KeyCode::from)
+                        .collect(),
+                ),
+                timeout,
+            })),
+        ),
+    )))
 }
 
 /// Creates a `KeyOutputs` from `layers::LAYERS`.
---
+++ src/kanata/caps_word.rs
@@ -0,0 +1,64 @@
+use kanata_keyberon::key_code::KeyCode;
+use rustc_hash::FxHashSet as HashSet;
+
+use crate::custom_action::CapsWordCfg;
+
+#[derive(Debug)]
+pub struct CapsWordState {
+    /// Keys that will trigger an `lsft` key to be added to the active keys if present in the
+    /// currently active keys.
+    pub keys_to_capitalize: HashSet<KeyCode>,
+    /// An extra list of keys that should **not** terminate the caps_word state, in addition to
+    /// keys_to_capitalize, but which don't trigger a capitalization.
+    pub keys_nonterminal: HashSet<KeyCode>,
+    /// The configured timeout for caps_word.
+    pub timeout: u16,
+    /// The number of ticks remaining for caps_word, after which its state should be cleared. The
+    /// number of ticks gets reset back to `timeout` when `maybe_add_lsft` is called. The reason
+    /// for having this timeout at all is in case somebody was in the middle of typing a word, had
+    /// to go do something, and forgot that caps_word was active. Having this timeout means that
+    /// shift won't be active for their next keypress.
+    pub timeout_ticks: u16,
+}
+
+#[derive(PartialEq, Eq, Debug, Clone, Copy)]
+pub enum CapsWordNextState {
+    Active,
+    End,
+}
+
+use CapsWordNextState::*;
+
+impl CapsWordState {
+    pub(crate) fn new(cfg: &CapsWordCfg) -> Self {
+        Self {
+            keys_to_capitalize: cfg.keys_to_capitalize.iter().copied().collect(),
+            keys_nonterminal: cfg.keys_nonterminal.iter().copied().collect(),
+            timeout: cfg.timeout,
+            timeout_ticks: cfg.timeout,
+        }
+    }
+
+    pub(crate) fn maybe_add_lsft(&mut self, active_keys: &mut Vec<KeyCode>) -> CapsWordNextState {
+        if self.timeout_ticks == 0 {
+            return End;
+        }
+        for kc in active_keys.iter() {
+            if !self.keys_to_capitalize.contains(kc) && !self.keys_nonterminal.contains(kc) {
+                return End;
+            }
+        }
+        if active_keys
+            .last()
+            .map(|kc| self.keys_to_capitalize.contains(kc))
+            .unwrap_or(false)
+        {
+            active_keys.insert(0, KeyCode::LShift);
+        }
+        if !active_keys.is_empty() {
+            self.timeout_ticks = self.timeout;
+        }
+        self.timeout_ticks = self.timeout_ticks.saturating_sub(1);
+        Active
+    }
+}
