a49c179693dc02b7e5c400b99101f17eb25c706b
Jan Tache <j.andreitabs@gmail.com>
2023-08-19 21:45:35 -0700

    actually parse toggle into toggle

--- parser/src/cfg/mod.rs
+++ parser/src/cfg/mod.rs
@@ -2261,7 +2261,7 @@ fn parse_fake_key_op_coord_action(
             "tap" => Some(FakeKeyAction::Tap),
             "press" => Some(FakeKeyAction::Press),
             "release" => Some(FakeKeyAction::Release),
-            "toggle" => Some(FakeKeyAction::Release),
+            "toggle" => Some(FakeKeyAction::Toggle),
             _ => None,
         })
         .flatten()
