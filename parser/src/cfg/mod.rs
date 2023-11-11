e19e33584700a3c1f380a37ac48a22bff3dd8be4
rszyma <rszyma.dev@gmail.com>
2023-11-11 07:35:22 +0100

    parser: another error message hint

--- parser/src/cfg/mod.rs
+++ parser/src/cfg/mod.rs
@@ -1271,9 +1271,11 @@ fn parse_action_atom(ac_span: &Spanned<String>, s: &ParsedState) -> Result<&'sta
             .ok_or_else(|| {
                 // check aliases
                 if s.aliases.contains_key(ac) {
-                    anyhow!("Unknown key/action: {ac:?}. If you meant to use an alias, prefix it with '@' symbol: @{ac}")
+                    anyhow!("Unknown key/action: {ac}. If you meant to use an alias, prefix it with '@' symbol: @{ac}")
+                } else if s.vars.contains_key(ac) {
+                    anyhow!("Unknown key/action: {ac}. If you meant to use a variable, prefix it with '$' symbol: ${ac}")
                 } else {
-                    anyhow!("Unknown key/action: {ac:?}")
+                    anyhow!("Unknown key/action: {ac}")
                 }
             })?
             .into(),
