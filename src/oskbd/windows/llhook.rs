70fc97d97c55e8c99e835168c56bffec9be524ff
eugenesvk <eugenesvk@users.noreply.github.com>
2024-03-16 21:22:02 +0700

    better output print for simulated events
    new: out:↑RAlt
    old: out:InputEvent { code: 165, up: true }

--- src/oskbd/windows/llhook.rs
+++ src/oskbd/windows/llhook.rs
@@ -3,6 +3,7 @@
 // This file is taken from kbremap with minor modifications.
 // https://github.com/timokroeger/kbremap
 
+use core::fmt;
 use std::cell::Cell;
 use std::io;
 use std::{mem, ptr};
@@ -14,6 +15,7 @@ use winapi::um::winuser::*;
 
 use crate::kanata::CalculatedMouseMove;
 use crate::oskbd::{KeyEvent, KeyValue};
+use kanata_keyberon::key_code::KeyCode;
 use kanata_parser::custom_action::*;
 use kanata_parser::keys::*;
 
@@ -71,6 +73,14 @@ pub struct InputEvent {
 
     /// Key was released
     pub up: bool,
+}
+
+impl fmt::Display for InputEvent {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let direction = if self.up { "↑" } else { "↓" };
+        let key_name = KeyCode::from(OsCode::from(self.code));
+        write!(f, "{}{:?}", direction, key_name)
+    }
 }
 
 impl InputEvent {
