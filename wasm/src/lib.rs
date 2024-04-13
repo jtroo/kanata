a1cc1df07b81437b8f8ffdc0626ba70e4db405b2
jtroo <j.andreitabs@gmail.com>
2024-04-13 15:02:09 -0700

    refactor(wasm): remove wasm-logger

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -10,7 +10,6 @@ static INIT: Once = Once::new();
 pub fn init() {
     INIT.call_once(|| {
         std::panic::set_hook(Box::new(console_error_panic_hook::hook));
-        wasm_logger::init(wasm_logger::Config::default());
     });
 }
 
