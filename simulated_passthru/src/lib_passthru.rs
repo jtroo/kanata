a5176e2deb968f4d9e776236022a7cfe6f40bfa4
eugenesvk <eugenesvk@users.noreply.github.com>
2024-04-14 21:40:45 +0700

    refactor(lib_passthru): move key_in/out to modules

--- simulated_passthru/src/lib_passthru.rs
+++ simulated_passthru/src/lib_passthru.rs
@@ -54,47 +54,12 @@ fn lib_impl() -> Result<()> {
 use kanata_parser::keys::OsCode;
 use std::panic::catch_unwind;
 
+mod key_in;
+mod key_out;
+use crate::key_in::*;
+use crate::key_out::*;
 
-use crate::oskbd::OUTEVWRAP;
-/// Receives the address of the external app's callback function that accepts simulated output events
-#[cfg(    feature = "passthru_ahk")]
-fn set_out_ev_listener(cb_addr:c_longlong) -> LRESULT { //c_int = i32 c_longlong=i64
-  // cbKanataOut(vk,sc,up) {return 1}: // All args are i64 (ahk doesn't support u64)
-  // address: pointer-sized integer, equivalent to Int64 on ahk64
-  // AHK uses x64 calling convention: todo: is this the same as win64? extern "C" also seems to work
-  log::trace!("@set_out_ev_listener: got func address {}",cb_addr);
-  let ptr_fn = cb_addr as *const (); // `as`-cast to a raw pointer before `transmute`ing to a function pointer. This avoids an integer-to-pointer `transmute`, which can be problematic. Transmuting between raw pointers and function pointers (i.e., two pointer types) is fine.
-  let cb_out_ev = unsafe {std::mem::transmute::<*const (), fn(vk:i64,sc:i64,up:i64) -> i64>(ptr_fn)};
-  OUTEVWRAP.get_or_init(|| {FnOutEvWrapper {cb:Arc::new(cb_out_ev)}});
-  0
-}
-#[cfg(not(feature = "passthru_ahk"))]
-fn set_out_ev_listener(cb_addr:c_longlong) -> LRESULT { //c_int = i32 c_longlong=i64
-  debug!("✗✗✗✗ unimplemented!");
-  unimplemented!();
-  0
-}
-
-use crate::oskbd::HOOK_CB;
-/// Exported function: receives key input and uses event_loop's input event handler callback (which will in turn communicate via the internal kanata's channels to keyberon state machine etc.)
-#[no_mangle] pub extern "win64" fn input_ev_listener(vk:c_uint, sc:c_uint, up:c_int) -> LRESULT {
-  #[cfg(feature="perf_logging")] let start = std::time::Instant::now();
-  let key_event	= InputEvent::from_vk_sc(vk,sc,up); //{code:KEY_0,value:Press}
-
-  let mut h_cbl	= HOOK_CB.lock(); // to access the closure we move its box out of the mutex and put it back after it returned
-  if let Some(mut fnhook) = h_cbl.take() { // move our opt+boxed closure, replacing it with None, can't just .unwrap since Copy trait not implemented for dyn fnMut
-    let handled = fnhook(key_event); // box(closure)() = closure()
-    *h_cbl = Some(fnhook); // put our closure back
-    if handled {
-      #[cfg(    feature="perf_logging") ]
-      debug!(" 🕐{}μs   →→→✓ {key_event} from {vk} sc={sc} up={up}",(start.elapsed()).as_micros());
-      #[cfg(not(feature="perf_logging"))]
-      debug!("   →→→✓ {key_event} from {vk} sc={sc} up={up}");
-      1} else {0}
-  } else {log::error!("fnHook processing key events isn't available yet"); 0}
-}
-
-use log::{debug, info};
+use log::*;
 mod log_win;
 #[no_mangle] pub extern "win64"
 fn lib_kanata_passthru(cb_addr:c_longlong) -> LRESULT {
