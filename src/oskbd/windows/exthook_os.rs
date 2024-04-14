cc973b8e0b6a5fa39b3970c5e3781ceff3846de7
eugenesvk <eugenesvk@users.noreply.github.com>
2024-04-15 01:46:00 +0700

    libpassthru: move input_ev_listener from exthook_os to passthru

--- src/oskbd/windows/exthook_os.rs
+++ src/oskbd/windows/exthook_os.rs
@@ -23,7 +23,7 @@ pub const LLHOOK_IDLE_TIME_CLEAR_INPUTS: u64 = 60;
 
 type HookFn = dyn FnMut(InputEvent) -> bool + Send + Sync + 'static;
 
-static HOOK_CB:Lazy<Mutex<Option<Box<HookFn>>>> = Lazy::new(|| Mutex::new(None)); // store thread-safe hook callback with a mutex (can be called from an external process)
+pub static HOOK_CB:Lazy<Mutex<Option<Box<HookFn>>>> = Lazy::new(|| Mutex::new(None)); // store thread-safe hook callback with a mutex (can be called from an external process)
 
 pub struct KeyboardHook {}  // reusing hook type for our listener
 impl KeyboardHook { /// Sets input callback (panics if already registered)
@@ -65,21 +65,3 @@ impl From<KeyEvent> for InputEvent {
   fn from(item: KeyEvent) -> Self { Self {
     code: item.code .into(),
     up  : item.value.into(),} }   }
-
-/// Exported function: receives key input and uses event_loop's input event handler callback (which will in turn communicate via the internal kanata's channels to keyberon state machine etc.)
-#[no_mangle] pub extern "C" fn input_ev_listener(vk:c_uint, sc:c_uint, up:c_int) -> LRESULT {
-  #[cfg(feature="perf_logging")] let start = std::time::Instant::now();
-  let key_event	= InputEvent::from_vk_sc(vk,sc,up); //{code:KEY_0,value:Press}
-
-  let mut h_cbl	= HOOK_CB.lock(); // to access the closure we move its box out of the mutex and put it back after it returned
-  if let Some(mut fnhook) = h_cbl.take() { // move our opt+boxed closure, replacing it with None, can't just .unwrap since Copy trait not implemented for dyn fnMut
-    let handled = fnhook(key_event); // box(closure)() = closure()
-    *h_cbl = Some(fnhook); // put our closure back
-    if handled {
-      #[cfg(    feature="perf_logging") ]
-      debug!(" 🕐{}μs   →→→✓ {key_event} from {vk} sc={sc} up={up}",(start.elapsed()).as_micros());
-      #[cfg(not(feature="perf_logging"))]
-      debug!("   →→→✓ {key_event} from {vk} sc={sc} up={up}");
-      1} else {0}
-  } else {log::error!("fnHook processing key events isn't available yet"); 0}
-}
