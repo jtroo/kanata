0682b0fd27b625105122a09abce85c31be987931
eugenesvk <eugenesvk@users.noreply.github.com>
2024-04-19 22:52:16 +0700

    win-tray: update import

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,10 @@ use std::str::FromStr;
 pub mod kanata;
 pub mod oskbd;
 pub mod tcp_server;
+#[cfg(feature = "gui")]
+pub mod m_gui;
+#[cfg(feature = "gui")]
+use win_dbg_logger as log_win;
 
 pub use kanata::*;
 pub use tcp_server::TcpServer;
