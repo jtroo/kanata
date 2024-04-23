3deba08db6b9d3ea960644ae5a8bc8857215847d
eugenesvk <eugenesvk@users.noreply.github.com>
2024-04-23 15:52:50 +0700

    win-tray: add dpi awareness to the manifest

--- build.rs
+++ build.rs
@@ -103,6 +103,12 @@ mod windows {
                   <requestedPrivileges><requestedExecutionLevel level="asInvoker" uiAccess="false"/></requestedPrivileges>
                 </security>
               </trustInfo>
+              <asmv3:application>
+                <asmv3:windowsSettings>
+                  <dpiAware     xmlns="http://schemas.microsoft.com/SMI/2005/WindowsSettings">true</dpiAware>
+                  <dpiAwareness xmlns="http://schemas.microsoft.com/SMI/2016/WindowsSettings">PerMonitorV2</dpiAwareness>
+                </asmv3:windowsSettings>
+              </asmv3:application>
               <dependency><dependentAssembly>
                 <assemblyIdentity type="win32" name="Microsoft.Windows.Common-Controls"
                   version="6.0.0.0" processorArchitecture="*" publicKeyToken="6595b64144ccf1df" language="*"/></dependentAssembly>
