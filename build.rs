79fa0ec5cfc8d9a6b413a63176e243b8ac46ee95
eugenesvk <eugenesvk@users.noreply.github.com>
2024-04-23 16:19:18 +0700

    gui-tray: fix manifest format

--- build.rs
+++ build.rs
@@ -96,23 +96,19 @@ mod windows {
 
         let manifest_str = formatdoc!(
             r#"<?xml version="1.0" encoding="utf-8" standalone="yes"?>
-            <assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
-              <assemblyIdentity name="kanata.exe" version="{}" type="win32"/>
-              <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
-                <security>
-                  <requestedPrivileges><requestedExecutionLevel level="asInvoker" uiAccess="false"/></requestedPrivileges>
-                </security>
-              </trustInfo>
-              <asmv3:application>
-                <asmv3:windowsSettings>
+            <assembly manifestVersion="1.0" xmlns:v1="urn:schemas-microsoft-com:asm.v1" xmlns="urn:schemas-microsoft-com:asm.v3">
+              <v1:assemblyIdentity name="kanata.exe" version="{}" type="win32"/>
+              <trustInfo><security>
+                <requestedPrivileges><requestedExecutionLevel level="asInvoker" uiAccess="false"/></requestedPrivileges>
+              </security></trustInfo>
+              <application><windowsSettings>
                   <dpiAware     xmlns="http://schemas.microsoft.com/SMI/2005/WindowsSettings">true</dpiAware>
                   <dpiAwareness xmlns="http://schemas.microsoft.com/SMI/2016/WindowsSettings">PerMonitorV2</dpiAwareness>
-                </asmv3:windowsSettings>
-              </asmv3:application>
-              <dependency><dependentAssembly>
-                <assemblyIdentity type="win32" name="Microsoft.Windows.Common-Controls"
-                  version="6.0.0.0" processorArchitecture="*" publicKeyToken="6595b64144ccf1df" language="*"/></dependentAssembly>
-              </dependency>
+              </windowsSettings></application>
+              <v1:dependency><v1:dependentAssembly>
+                <v1:assemblyIdentity type="win32" name="Microsoft.Windows.Common-Controls"
+                  version="6.0.0.0" processorArchitecture="*" publicKeyToken="6595b64144ccf1df" language="*"/></v1:dependentAssembly>
+              </v1:dependency>
             </assembly>
             "#,
             version
