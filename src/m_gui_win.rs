781c492aafc7db90757a5f0dceb39a37ac5d4c19
eugenesvk <eugenesvk@users.noreply.github.com>
2024-04-23 17:23:52 +0700

    win-tray: embed icon

--- src/m_gui_win.rs
+++ src/m_gui_win.rs
@@ -7,18 +7,21 @@ use nwd::NwgUi;
 use nwg::NativeUi;
 
 #[derive(Default, NwgUi)] pub struct SystemTray {
-  #[nwg_control]                                           	window	: nwg::MessageWindow,
-  #[nwg_resource(source_file:Some("../assets/kanata.ico"))]	icon  	: nwg::Icon,
-  #[nwg_control(icon:Some(&data.icon), tip: Some("Hello"))]	//
-   #[nwg_events(MousePressLeftUp:[SystemTray::show_menu]   	//
-    ,           OnContextMenu   :[SystemTray::show_menu])] 	tray     	: nwg::TrayNotification,
-  #[nwg_control(parent:window   , popup: true)]            	tray_menu	: nwg::Menu,
-  #[nwg_control(parent:tray_menu, text:"&1 Hello")]        	//
-   #[nwg_events(OnMenuItemSelected:[SystemTray::hello1])]  	tray_item1	: nwg::MenuItem,
-  #[nwg_control(parent:tray_menu, text:"&2 Popup")]        	//
-   #[nwg_events(OnMenuItemSelected:[SystemTray::hello2])]  	tray_item2	: nwg::MenuItem,
-  #[nwg_control(parent:tray_menu, text:"&X Exit")]         	//
-   #[nwg_events(OnMenuItemSelected:[SystemTray::exit  ])]  	tray_item3	: nwg::MenuItem,
+  #[nwg_resource]                                         	embed     	: nwg::EmbedResource,
+  #[nwg_control]                                          	window    	: nwg::MessageWindow,
+  #[nwg_resource(source_embed    :Some(&data.embed)       	          	//
+    ,            source_embed_str:Some("iconMain"))]      	icon      	: nwg::Icon,
+  #[nwg_control(icon:Some(&data.icon)                     	          	//
+    ,           tip :Some("Hello"))]                      	          	//
+   #[nwg_events(MousePressLeftUp:[SystemTray::show_menu]  	          	//
+    ,           OnContextMenu   :[SystemTray::show_menu])]	tray      	: nwg::TrayNotification,
+  #[nwg_control(parent:window   , popup: true)]           	tray_menu 	: nwg::Menu,
+  #[nwg_control(parent:tray_menu, text:"&1 Hello")]       	          	//
+   #[nwg_events(OnMenuItemSelected:[SystemTray::hello1])] 	tray_item1	: nwg::MenuItem,
+  #[nwg_control(parent:tray_menu, text:"&2 Popup")]       	          	//
+   #[nwg_events(OnMenuItemSelected:[SystemTray::hello2])] 	tray_item2	: nwg::MenuItem,
+  #[nwg_control(parent:tray_menu, text:"&X Exit")]        	          	//
+   #[nwg_events(OnMenuItemSelected:[SystemTray::exit  ])] 	tray_item3	: nwg::MenuItem,
 }
 impl SystemTray {
   fn show_menu(&self) {
