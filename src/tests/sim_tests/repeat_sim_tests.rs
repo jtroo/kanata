jtroo/kanata

commit 089853a77bf240e8b1136b8afd5c9e867f189e58

refactor(layers): remove base/held duplication (#934)

src/tests/sim_tests/mod.rs
@@ -1,0 +1,53 @@
+use crate::tests::*;
+use kanata_state_machine::{
+    oskbd::{KeyEvent, KeyValue},
+    str_to_oscode, Kanata,
+};
+
+mod chord_sim_tests;
+mod layer_sim_tests;
+
+fn simulate(cfg: &str, sim: &str) -> String {
+    let _lk = match CFG_PARSE_LOCK.lock() {
+        Ok(guard) => guard,
+        Err(poisoned) => poisoned.into_inner(),
+    };
+    let mut k = Kanata::new_from_str(cfg).expect("failed to parse cfg");
+    for pair in sim.split_whitespace() {
+        match pair.split_once(':') {
+            Some((kind, val)) => match kind {
+                "t" => {
+                    let tick = str::parse::<u128>(val).expect("valid num for tick");
+                    k.tick_ms(tick, &None).unwrap();
+                }
+                "d" => {
+                    let key_code = str_to_oscode(val).expect("valid keycode");
+                    k.handle_input_event(&KeyEvent {
+                        code: key_code,
+                        value: KeyValue::Press,
+                    })
+                    .expect("input handles fine");
+                }
+                "u" => {
+                    let key_code = str_to_oscode(val).expect("valid keycode");
+                    k.handle_input_event(&KeyEvent {
+                        code: key_code,
+                        value: KeyValue::Release,
+                    })
+                    .expect("input handles fine");
+                }
+                "r" => {
+                    let key_code = str_to_oscode(val).expect("valid keycode");
+                    k.handle_input_event(&KeyEvent {
+                        code: key_code,
+                        value: KeyValue::Repeat,
+                    })
+                    .expect("input handles fine");
+                }
+                _ => panic!("invalid item {pair}"),
+            },
+            None => panic!("invalid item {pair}"),
+        }
+    }
+    k.kbd_out.outputs.events.join("\n")
+}

parser/src/cfg/mod.rs
@@ -1600,13 +1600,11 @@ fn parse_action_list(ac: &[SExpr], s: &ParserState) -> Result<&'static KanataAct
 }
 
 fn parse_layer_base(ac_params: &[SExpr], s: &ParserState) -> Result<&'static KanataAction> {
-    Ok(s.a.sref(Action::DefaultLayer(
-        layer_idx(ac_params, &s.layer_idxs)? * 2,
-    )))
+    Ok(s.a.sref(Action::DefaultLayer(layer_idx(ac_params, &s.layer_idxs)?)))
 }
 
 fn parse_layer_toggle(ac_params: &[SExpr], s: &ParserState) -> Result<&'static KanataAction> {
-    Ok(s.a.sref(Action::Layer(layer_idx(ac_params, &s.layer_idxs)? * 2 + 1)))
+    Ok(s.a.sref(Action::Layer(layer_idx(ac_params, &s.layer_idxs)?)))
 }
 
 fn layer_idx(ac_params: &[SExpr], layers: &LayerIndexes) -> Result<usize> {
@@ -2285,9 +2283,11 @@ fn parse_release_key(ac_params: &[SExpr], s: &ParserState) -> Result<&'static Ka
 }
 
 fn parse_release_layer(ac_params: &[SExpr], s: &ParserState) -> Result<&'static KanataAction> {
-    Ok(s.a.sref(Action::ReleaseState(ReleasableState::Layer(
-        layer_idx(ac_params, &s.layer_idxs)? * 2 + 1,
-    ))))
+    Ok(s.a
+        .sref(Action::ReleaseState(ReleasableState::Layer(layer_idx(
+            ac_params,
+            &s.layer_idxs,
+        )?))))
 }
 
 fn create_defsrc_layer() -> [KanataAction; KEYS_IN_ROW] {
@@ -2813,7 +2813,7 @@ fn parse_live_reload_file(ac_params: &[SExpr], s: &ParserState) -> Result<&'stat
 }
 
 fn parse_layers(
-    s: &ParserState,
+    s: &mut ParserState,
     mapped_keys: &mut MappedKeys,
     defcfg: &CfgOptions,
 ) -> Result<IntermediateLayers> {
@@ -2832,8 +2832,7 @@ fn parse_layers(
                 // to defsrc mapping order.
                 for (i, ac) in layer.iter().skip(2).enumerate() {
                     let ac = parse_action(ac, s)?;
-                    layers_cfg[layer_level * 2][0][s.mapping_order[i]] = *ac;
-                    layers_cfg[layer_level * 2 + 1][0][s.mapping_order[i]] = *ac;
+                    layers_cfg[layer_level][0][s.mapping_order[i]] = *ac;
                 }
             }
             LayerExprs::CustomMapping(layer) => {
@@ -2868,9 +2867,8 @@ fn parse_layers(
                             bail_expr!(input, "must either use _ or ___ within a layer, not both")
                         }
                         for i in 0..s.mapping_order.len() {
-                            if layers_cfg[layer_level * 2][0][s.mapping_order[i]] == Action::Trans {
-                                layers_cfg[layer_level * 2][0][s.mapping_order[i]] = *action;
-                                layers_cfg[layer_level * 2 + 1][0][s.mapping_order[i]] = *action;
+                            if layers_cfg[layer_level][0][s.mapping_order[i]] == Action::Trans {
+                                layers_cfg[layer_level][0][s.mapping_order[i]] = *action;
                             }
                         }
                         defsrc_anykey_used = true;
@@ -2888,11 +2886,10 @@ fn parse_layers(
                             bail_expr!(input, "must either use __ or ___ within a layer, not both")
                         }
                         for i in 0..layers_cfg[0][0].len() {
-                            if layers_cfg[layer_level * 2][0][i] == Action::Trans
+                            if layers_cfg[layer_level][0][i] == Action::Trans
                                 && !s.mapping_order.contains(&i)
                             {
-                                layers_cfg[layer_level * 2][0][i] = *action;
-                                layers_cfg[layer_level * 2 + 1][0][i] = *action;
+                                layers_cfg[layer_level][0][i] = *action;
                             }
                         }
                         unmapped_anykey_used = true;
@@ -2913,9 +2910,8 @@ fn parse_layers(
                             );
                         }
                         for i in 0..layers_cfg[0][0].len() {
-                            if layers_cfg[layer_level * 2][0][i] == Action::Trans {
-                                layers_cfg[layer_level * 2][0][i] = *action;
-                                layers_cfg[layer_level * 2 + 1][0][i] = *action;
+                            if layers_cfg[layer_level][0][i] == Action::Trans {
+                                layers_cfg[layer_level][0][i] = *action;
                             }
                         }
                         both_anykey_used = true;
@@ -2928,8 +2924,7 @@ fn parse_layers(
                         if !layer_mapped_keys.insert(input_key) {
                             bail_expr!(input, "input key must not be repeated within a layer")
                         }
-                        layers_cfg[layer_level * 2][0][usize::from(input_key)] = *action;
-                        layers_cfg[layer_level * 2 + 1][0][usize::from(input_key)] = *action;
+                        layers_cfg[layer_level][0][usize::from(input_key)] = *action;
                     }
                 }
                 let rem = pairs.remainder();
@@ -2948,23 +2943,15 @@ fn parse_layers(
                 }
             }
         }
-        for (layer_action, defsrc_action) in
-            layers_cfg[layer_level * 2][0].iter_mut().zip(defsrc_layer)
-        {
-            // Set transparent actions in the "layer-switch" version of the layer according to
-            // defsrc action.
-            if *layer_action == Action::Trans {
-                if s.block_unmapped_keys {
-                    *layer_action = Action::NoOp;
-                } else {
-                    *layer_action = defsrc_action;
-                }
+        for layer_action in layers_cfg[layer_level][0].iter_mut() {
+            if *layer_action == Action::Trans && s.block_unmapped_keys {
+                *layer_action = Action::NoOp;
             }
         }
         // Set fake keys on the `layer-switch` version of each layer.
         for (y, action) in s.virtual_keys.values() {
             let (x, y) = get_fake_key_coords(*y);
-            layers_cfg[layer_level * 2][x as usize][y as usize] = **action;
+            layers_cfg[layer_level][x as usize][y as usize] = **action;
         }
 
         // If the user has configured delegation to the first (default) layer for transparent keys,
@@ -2972,10 +2959,13 @@ fn parse_layers(
         // the first layer.
         if layer_level == 0 && s.delegate_to_first_layer {
             for (defsrc_ac, default_layer_ac) in defsrc_layer.iter_mut().zip(layers_cfg[0][0]) {
-                *defsrc_ac = default_layer_ac;
+                if default_layer_ac != Action::Trans {
+                    *defsrc_ac = default_layer_ac;
+                }
             }
         }
     }
+    s.defsrc_layer = defsrc_layer;
     Ok(layers_cfg)
 }
 

parser/src/cfg/chord.rs
@@ -74,8 +74,7 @@ pub(crate) fn parse_defchordv2(
                             .atom(s.vars())
                             .and_then(|l| s.layer_idxs.get(l))
                             .ok_or_else(|| anyhow_expr!(layer, "Not a known layer name."))?;
-                        layers.push((*l_idx * 2) as u16);
-                        layers.push((*l_idx * 2 + 1) as u16);
+                        layers.push((*l_idx) as u16);
                         Ok(layers)
                     })
             })

parser/src/cfg/tests.rs
@@ -216,144 +216,6 @@ fn parse_action_vars() {
 }
 
 #[test]
-fn parse_delegate_to_default_layer_yes() {
-    let _lk = match CFG_PARSE_LOCK.lock() {
-        Ok(guard) => guard,
-        Err(poisoned) => poisoned.into_inner(),
-    };
-    let mut s = ParserState::default();
-    let source = r#"
-(defcfg delegate-to-first-layer yes)
-(defsrc a)
-(deflayer base b)
-(deflayer other _)
-"#;
-    let res = parse_cfg_raw_string(
-        source,
-        &mut s,
-        &PathBuf::from("test"),
-        &mut FileContentProvider {
-            get_file_content_fn: &mut |_| unimplemented!(),
-        },
-        DEF_LOCAL_KEYS,
-        Err("env vars not implemented".into()),
-    )
-    .unwrap();
-    assert_eq!(
-        res.klayers[2][0][OsCode::KEY_A.as_u16() as usize],
-        Action::KeyCode(KeyCode::B),
-    );
-}
-
-#[test]
-fn parse_delegate_to_default_layer_yes_but_base_transparent() {
-    let _lk = match CFG_PARSE_LOCK.lock() {
-        Ok(guard) => guard,
-        Err(poisoned) => poisoned.into_inner(),
-    };
-    let mut s = ParserState::default();
-    let source = r#"
-(defcfg delegate-to-first-layer yes)
-(defsrc a)
-(deflayer base _)
-(deflayer other _)
-"#;
-    let res = parse_cfg_raw_string(
-        source,
-        &mut s,
-        &PathBuf::from("test"),
-        &mut FileContentProvider {
-            get_file_content_fn: &mut |_| unimplemented!(),
-        },
-        DEF_LOCAL_KEYS,
-        Err("env vars not implemented".into()),
-    )
-    .unwrap();
-    assert_eq!(
-        res.klayers[2][0][OsCode::KEY_A.as_u16() as usize],
-        Action::KeyCode(KeyCode::A),
-    );
-}
-
-#[test]
-fn parse_delegate_to_default_layer_no() {
-    let _lk = match CFG_PARSE_LOCK.lock() {
-        Ok(guard) => guard,
-        Err(poisoned) => poisoned.into_inner(),
-    };
-    let mut s = ParserState::default();
-    let source = r#"
-(defcfg delegate-to-first-layer no)
-(defsrc a)
-(deflayer base b)
-(deflayer other _)
-"#;
-    let res = parse_cfg_raw_string(
-        source,
-        &mut s,
-        &PathBuf::from("test"),
-        &mut FileContentProvider {
-            get_file_content_fn: &mut |_| unimplemented!(),
-        },
-        DEF_LOCAL_KEYS,
-        Err("env vars not implemented".into()),
-    )
-    .unwrap();
-    assert_eq!(
-        res.klayers[2][0][OsCode::KEY_A.as_u16() as usize],
-        Action::KeyCode(KeyCode::A),
-    );
-}
-
-#[test]
-fn parse_transparent_default() {
-    let _lk = match CFG_PARSE_LOCK.lock() {
-        Ok(guard) => guard,
-        Err(poisoned) => poisoned.into_inner(),
-    };
-    let mut s = ParserState::default();
-    let icfg = parse_cfg_raw(
-        &std::path::PathBuf::from("./test_cfgs/transparent_default.kbd"),
-        &mut s,
-    )
-    .unwrap();
-    let layers = icfg.klayers;
-
-    assert_eq!(icfg.layer_info.len(), 4);
-
-    assert_eq!(
-        layers[0][0][usize::from(OsCode::KEY_F13)],
-        Action::KeyCode(KeyCode::F13)
-    );
-    assert_eq!(
-        layers[0][0][usize::from(OsCode::KEY_F14)],
-        Action::DefaultLayer(2)
-    );
-    assert_eq!(layers[0][0][usize::from(OsCode::KEY_F15)], Action::Layer(3));
-    assert_eq!(layers[1][0][usize::from(OsCode::KEY_F13)], Action::Trans);
-    assert_eq!(
-        layers[1][0][usize::from(OsCode::KEY_F14)],
-        Action::DefaultLayer(2)
-    );
-    assert_eq!(layers[1][0][usize::from(OsCode::KEY_F15)], Action::Layer(3));
-    assert_eq!(
-        layers[2][0][usize::from(OsCode::KEY_F13)],
-        Action::DefaultLayer(0)
-    );
-    assert_eq!(layers[2][0][usize::from(OsCode::KEY_F14)], Action::Layer(1));
-    assert_eq!(
-        layers[2][0][usize::from(OsCode::KEY_F15)],
-        Action::KeyCode(KeyCode::F15)
-    );
-    assert_eq!(
-        layers[3][0][usize::from(OsCode::KEY_F13)],
-        Action::DefaultLayer(0)
-    );
-    assert_eq!(layers[3][0][usize::from(OsCode::KEY_F14)], Action::Layer(1));
-    assert_eq!(layers[3][0][usize::from(OsCode::KEY_F15)], Action::Trans);
-}
-
-#[test]
 fn parse_multiline_comment() {
     let _lk = match CFG_PARSE_LOCK.lock() {
         Ok(guard) => guard,

parser/src/layers.rs
@@ -17,7 +17,7 @@ pub type Row = [kanata_keyberon::action::Action<'static, &'static &'static [&'st
     KEYS_IN_ROW];
 
 pub fn new_layers(layers: usize) -> IntermediateLayers {
-    let actual_num_layers = layers * 2;
+    let actual_num_layers = layers;
     // Note: why construct it like this?
     // Because don't want to construct KanataLayers on the stack.
     // The stack will overflow because of lack of placement new.

parser/test_cfgs/transparent_default.kbd
@@ -1,16 +1,0 @@
-(defcfg
-  linux-dev /dev/input/by-path/platform-i8042-serio-0-event-kbd
-)
-
-(defsrc
-       f13  f14  f15
-)
-
-(deflayer default
-       _ (layer-switch test) (layer-toggle test)
-)
-
-(deflayer test
-       (layer-switch default) (layer-toggle default) _
-)
-

src/tests/sim_tests/chord_sim_tests.rs
@@ -1,55 +1,5 @@
 use super::*;
 
-use kanata_state_machine::{
-    oskbd::{KeyEvent, KeyValue},
-    str_to_oscode, Kanata,
-};
-
-fn simulate(cfg: &str, sim: &str) -> String {
-    let _lk = match CFG_PARSE_LOCK.lock() {
-        Ok(guard) => guard,
-        Err(poisoned) => poisoned.into_inner(),
-    };
-    let mut k = Kanata::new_from_str(cfg).expect("failed to parse cfg");
-    for pair in sim.split_whitespace() {
-        match pair.split_once(':') {
-            Some((kind, val)) => match kind {
-                "t" => {
-                    let tick = str::parse::<u128>(val).expect("valid num for tick");
-                    k.tick_ms(tick, &None).unwrap();
-                }
-                "d" => {
-                    let key_code = str_to_oscode(val).expect("valid keycode");
-                    k.handle_input_event(&KeyEvent {
-                        code: key_code,
-                        value: KeyValue::Press,
-                    })
-                    .expect("input handles fine");
-                }
-                "u" => {
-                    let key_code = str_to_oscode(val).expect("valid keycode");
-                    k.handle_input_event(&KeyEvent {
-                        code: key_code,
-                        value: KeyValue::Release,
-                    })
-                    .expect("input handles fine");
-                }
-                "r" => {
-                    let key_code = str_to_oscode(val).expect("valid keycode");
-                    k.handle_input_event(&KeyEvent {
-                        code: key_code,
-                        value: KeyValue::Repeat,
-                    })
-                    .expect("input handles fine");
-                }
-                _ => panic!("invalid item {pair}"),
-            },
-            None => panic!("invalid item {pair}"),
-        }
-    }
-    k.kbd_out.outputs.events.join("\n")
-}
-
 static SIMPLE_NONOVERLAPPING_CHORD_CFG: &str = "\
 (defcfg process-unmapped-keys yes concurrent-tap-hold yes) \
 (defsrc) \

commit 4383c1677527ea7cd5944f15782dfda938b284d4

fix(repeat): check event code itself for repeat output
This commit fixes key repeat in Linux TTY and Windows when a key on the
base layer is transparent or unmapped, but process-unmapped-keys is
enabled.

This functionality was broken in commit 089853a which changed how layers
worked by removing the duplicated base-version and while-held-version of
layers, for the benefit of reduced memory consumption.

src/kanata/key_repeat.rs
@@ -1,0 +1,95 @@
+use super::*;
+
+impl Kanata {
+    /// This compares the active keys in the keyberon layout against the potential key outputs for
+    /// corresponding physical key in the configuration. If any of keyberon active keys match any
+    /// potential physical key output, write the repeat event to the OS.
+    pub(super) fn handle_repeat(&mut self, event: &KeyEvent) -> Result<()> {
+        let ret = self.handle_repeat_actual(event);
+        // The cur_keys Vec is re-used for processing, for efficiency reasons to avoid allocation.
+        // Unlike prev_keys which has useful info for the next call to handle_time_ticks, cur_keys
+        // can be reused and cleared — it just needs to be empty for the next handle_time_ticks
+        // call.
+        self.cur_keys.clear();
+        ret
+    }
+
+    pub(super) fn handle_repeat_actual(&mut self, event: &KeyEvent) -> Result<()> {
+        if self.sequence_state.is_some() {
+            // While in sequence mode, don't send key repeats. I can't imagine it's a helpful use
+            // case for someone trying to type in a sequence that they want to rely on key repeats
+            // to finish a sequence. I suppose one might want to do repeat in order to try and
+            // cancel an input sequence... I'll wait for a user created issue to deal with this.
+            return Ok(());
+        }
+        self.cur_keys.extend(self.layout.bm().keycodes());
+        self.overrides
+            .override_keys(&mut self.cur_keys, &mut self.override_states);
+
+        // Prioritize checking the active layer in case a layer-while-held is active.
+        let active_held_layers = self.layout.bm().trans_resolution_layer_order();
+        let mut held_layer_active = false;
+        for layer in active_held_layers {
+            held_layer_active = true;
+            if let Some(outputs_for_key) = self.key_outputs[usize::from(layer)].get(&event.code) {
+                log::debug!("key outs for active layer-while-held: {outputs_for_key:?};");
+                for osc in outputs_for_key.iter().rev().copied() {
+                    let kc = osc.into();
+                    if self.cur_keys.contains(&kc)
+                        || self.unshifted_keys.contains(&kc)
+                        || self.unmodded_keys.contains(&kc)
+                    {
+                        log::debug!("repeat    {:?}", KeyCode::from(osc));
+                        if let Err(e) = write_key(&mut self.kbd_out, osc, KeyValue::Repeat) {
+                            bail!("could not write key {e:?}")
+                        }
+                        return Ok(());
+                    }
+                }
+            }
+        }
+        if held_layer_active {
+            log::debug!("empty layer-while-held outputs, probably transparent");
+        }
+
+        if let Some(outputs_for_key) =
+            self.key_outputs[self.layout.bm().default_layer].get(&event.code)
+        {
+            // Try matching a key on the default layer.
+            //
+            // This code executes in two cases:
+            // 1. current layer is the default layer
+            // 2. current layer is layer-while-held but did not find a match in the code above, e.g. a
+            //    transparent key was pressed.
+            log::debug!("key outs for default layer: {outputs_for_key:?};");
+            for osc in outputs_for_key.iter().rev().copied() {
+                let kc = osc.into();
+                if self.cur_keys.contains(&kc)
+                    || self.unshifted_keys.contains(&kc)
+                    || self.unmodded_keys.contains(&kc)
+                {
+                    log::debug!("repeat    {:?}", KeyCode::from(osc));
+                    if let Err(e) = write_key(&mut self.kbd_out, osc, KeyValue::Repeat) {
+                        bail!("could not write key {e:?}")
+                    }
+                    return Ok(());
+                }
+            }
+        }
+
+        // Reached here and have not exited yet.
+        // Check the standard key output itself because default layer might also be transparent
+        // and have delegated to defsrc handling.
+        log::debug!("checking defsrc output");
+        let kc = event.code.into();
+        if self.cur_keys.contains(&kc)
+            || self.unshifted_keys.contains(&kc)
+            || self.unmodded_keys.contains(&kc)
+        {
+            if let Err(e) = write_key(&mut self.kbd_out, event.code, KeyValue::Repeat) {
+                bail!("could not write key {e:?}");
+            }
+        }
+        Ok(())
+    }
+}

src/kanata/mod.rs
@@ -20,6 +20,7 @@ use crate::tcp_server::simple_sexpr_to_json_array;
 use crate::SocketAddrWrapper;
 use crate::ValidatedArgs;
 use kanata_parser::cfg;
+use kanata_parser::cfg::list_actions::*;
 use kanata_parser::cfg::*;
 use kanata_parser::custom_action::*;
 pub use kanata_parser::keys::*;
@@ -28,7 +29,7 @@ use kanata_tcp_protocol::ServerMessage;
 mod dynamic_macro;
 use dynamic_macro::*;
 
-use kanata_parser::cfg::list_actions::*;
+mod key_repeat;
 
 #[cfg(feature = "cmd")]
 mod cmd;
@@ -1578,85 +1579,6 @@ impl Kanata {
 
         self.check_release_non_physical_shift()?;
         Ok(live_reload_requested)
-    }
-
-    /// This compares the active keys in the keyberon layout against the potential key outputs for
-    /// corresponding physical key in the configuration. If any of keyberon active keys match any
-    /// potential physical key output, write the repeat event to the OS.
-    fn handle_repeat(&mut self, event: &KeyEvent) -> Result<()> {
-        let ret = self.handle_repeat_actual(event);
-        // The cur_keys Vec is re-used for processing, for efficiency reasons to avoid allocation.
-        // Unlike prev_keys which has useful info for the next call to handle_time_ticks, cur_keys
-        // can be reused and cleared — it just needs to be empty for the next handle_time_ticks
-        // call.
-        self.cur_keys.clear();
-        ret
-    }
-
-    fn handle_repeat_actual(&mut self, event: &KeyEvent) -> Result<()> {
-        if self.sequence_state.is_some() {
-            // While in sequence mode, don't send key repeats. I can't imagine it's a helpful use
-            // case for someone trying to type in a sequence that they want to rely on key repeats
-            // to finish a sequence. I suppose one might want to do repeat in order to try and
-            // cancel an input sequence... I'll wait for a user created issue to deal with this.
-            return Ok(());
-        }
-        self.cur_keys.extend(self.layout.bm().keycodes());
-        self.overrides
-            .override_keys(&mut self.cur_keys, &mut self.override_states);
-
-        // Prioritize checking the active layer in case a layer-while-held is active.
-        let active_held_layers = self.layout.bm().active_held_layers();
-        let mut held_layer_active = false;
-        for layer in active_held_layers {
-            held_layer_active = true;
-            if let Some(outputs_for_key) = self.key_outputs[usize::from(layer)].get(&event.code) {
-                log::debug!("key outs for active layer-while-held: {outputs_for_key:?};");
-                for osc in outputs_for_key.iter().rev().copied() {
-                    let kc = osc.into();
-                    if self.cur_keys.contains(&kc)
-                        || self.unshifted_keys.contains(&kc)
-                        || self.unmodded_keys.contains(&kc)
-                    {
-                        log::debug!("repeat    {:?}", KeyCode::from(osc));
-                        if let Err(e) = write_key(&mut self.kbd_out, osc, KeyValue::Repeat) {
-                            bail!("could not write key {:?}", e)
-                        }
-                        return Ok(());
-                    }
-                }
-            }
-        }
-        if held_layer_active {
-            log::debug!("empty layer-while-held outputs, probably transparent");
-        }
-
-        // Try matching a key on the default layer.
-        //
-        // This code executes in two cases:
-        // 1. current layer is the default layer
-        // 2. current layer is layer-while-held but did not find a match in the code above, e.g. a
-        //    transparent key was pressed.
-        let outputs_for_key =
-            match self.key_outputs[self.layout.bm().default_layer].get(&event.code) {
-                None => return Ok(()),
-                Some(v) => v,
-            };
-        log::debug!("key outs for default layer: {outputs_for_key:?};");
-        for osc in outputs_for_key.iter().rev().copied() {
-            let kc = osc.into();
-            if self.cur_keys.contains(&kc)
-                || self.unshifted_keys.contains(&kc)
-                || self.unmodded_keys.contains(&kc)
-            {
-                log::debug!("repeat    {:?}", KeyCode::from(osc));
-                if let Err(e) = write_key(&mut self.kbd_out, osc, KeyValue::Repeat) {
-                    bail!("could not write key {:?}", e)
-                }
-                return Ok(());
-            }
-        }
-        Ok(())
     }
 
     #[cfg(feature = "tcp_server")]

src/tests/sim_tests/mod.rs
@@ -13,6 +13,7 @@ use kanata_state_machine::{
 mod block_keys_tests;
 mod chord_sim_tests;
 mod layer_sim_tests;
+mod repeat_sim_tests;
 mod seq_sim_tests;
 
 fn simulate(cfg: &str, sim: &str) -> String {

src/tests/sim_tests/repeat_sim_tests.rs
@@ -1,0 +1,136 @@
+use super::*;
+
+#[test]
+fn repeat_standard() {
+    let result = simulate(
+        "
+         (defsrc a)
+         (deflayer base b)
+        ",
+        "
+         d:a t:10 r:a t:10 r:a t:10 u:a t:10 r:a
+        ",
+    );
+    assert_eq!(
+        "out:↓B\nt:10ms\nout:↓B\nt:10ms\nout:↓B\nt:10ms\nout:↑B",
+        result
+    );
+}
+
+#[test]
+fn repeat_layer_while_held() {
+    let result = simulate(
+        "
+         (defsrc a b)
+         (deflayer base a (layer-while-held held))
+         (deflayer held c b)
+        ",
+        "
+         d:b t:10 r:b t:10 d:a t:10 r:a t:10 r:a t:10 u:a t:10 r:a
+        ",
+    );
+    assert_eq!(
+        "t:20ms\nout:↓C\nt:10ms\nout:↓C\nt:10ms\nout:↓C\nt:10ms\nout:↑C",
+        result
+    );
+}
+
+#[test]
+fn repeat_layer_switch() {
+    let result = simulate(
+        "
+         (defsrc a b)
+         (deflayer base a (layer-switch swtc))
+         (deflayer swtc d b)
+        ",
+        "
+         d:b t:10 r:b t:10 d:a t:10 r:a t:10 r:a t:10 u:a t:10 r:a
+        ",
+    );
+    assert_eq!(
+        "t:20ms\nout:↓D\nt:10ms\nout:↓D\nt:10ms\nout:↓D\nt:10ms\nout:↑D",
+        result
+    );
+}
+
+#[test]
+fn repeat_layer_held_trans() {
+    let result = simulate(
+        "
+         (defsrc a b)
+         (deflayer base e (layer-while-held held))
+         (deflayer held _ b)
+        ",
+        "
+         d:b t:10 r:b t:10 d:a t:10 r:a t:10 r:a t:10 u:a t:10 r:a
+        ",
+    );
+    assert_eq!(
+        "t:20ms\nout:↓E\nt:10ms\nout:↓E\nt:10ms\nout:↓E\nt:10ms\nout:↑E",
+        result
+    );
+}
+
+#[test]
+fn repeat_many_layer_held_trans() {
+    let result = simulate(
+        "
+         (defsrc a b c d e)
+         (deflayer base e (layer-while-held held1) _ _ _)
+         (deflayer held1 f b (layer-while-held held2) _ _)
+         (deflayer held2 _ _ _ (layer-while-held held3) _)
+         (deflayer held3 _ _ _ _ (layer-while-held held4))
+         (deflayer held4 _ _ _ _ _)
+        ",
+        "
+         d:b t:10 r:b t:10
+         d:c t:10 r:c t:10
+         d:d t:10 r:d t:10
+         d:e t:10 r:e t:10
+         d:a t:10 r:a t:10 r:a t:10 u:a t:10 r:a
+        ",
+    );
+    assert_eq!(
+        "t:80ms\nout:↓F\nt:10ms\nout:↓F\nt:10ms\nout:↓F\nt:10ms\nout:↑F",
+        result
+    );
+}
+
+#[test]
+fn repeat_base_layer_trans() {
+    let result = simulate(
+        "
+         (defsrc a)
+         (deflayer base _)
+        ",
+        "
+         d:a t:10 r:a t:10 r:a t:10 u:a t:10 r:a
+        ",
+    );
+    assert_eq!(
+        "out:↓A\nt:10ms\nout:↓A\nt:10ms\nout:↓A\nt:10ms\nout:↑A",
+        result
+    );
+}
+
+#[test]
+fn repeat_delegate_to_base_layer_trans() {
+    let result = simulate(
+        "
+         (defcfg delegate-to-first-layer yes)
+         (defsrc a c b)
+         (deflayer base e _ (layer-switch swtc))
+         (deflayer swtc _ _ _)
+        ",
+        "
+         d:b t:10 r:b t:10
+         d:a t:10 r:a t:10 r:a t:10 u:a t:10 r:a
+         d:c t:10 r:c t:10 r:c t:10 u:c t:10 r:c
+        ",
+    );
+    assert_eq!(
+        "t:20ms\nout:↓E\nt:10ms\nout:↓E\nt:10ms\nout:↓E\nt:10ms\nout:↑E\n\
+         t:10ms\nout:↓C\nt:10ms\nout:↓C\nt:10ms\nout:↓C\nt:10ms\nout:↑C",
+        result
+    );
+}