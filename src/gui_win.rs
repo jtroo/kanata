a5e2c53fc563d3f734954986218904a39f9c2579
eugenesvk <eugenesvk@users.noreply.github.com>
2024-05-04 02:31:43 +0700

    gui: reset menu item image cache as well on config reload

--- src/gui_win.rs
+++ src/gui_win.rs
@@ -520,9 +520,11 @@ impl SystemTray {
     ) {
         let mut icon_dyn = self.icon_dyn.borrow_mut(); // update the tray icon
         let mut icon_active = self.icon_active.borrow_mut(); // update the tray icon active path
+        let mut img_dyn     = self.img_dyn    .borrow_mut(); // update the tray images
         if clear {
             *icon_dyn = Default::default();
             *icon_active = Default::default();
+            *img_dyn = Default::default();
             debug!("reloading active config, clearing icon_dyn/_active cache");
         }
         let app_data = self.app_data.borrow();
