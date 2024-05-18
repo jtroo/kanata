34f80b22b0014eb531ec999e809aab541ef06305
eugenesvk <eugenesvk@users.noreply.github.com>
2024-05-18 21:08:49 +0700

    gui: update win_dbg_logger to be a module

--- src/gui/win_dbg_logger/mod.rs
+++ src/gui/win_dbg_logger/mod.rs
@@ -93,7 +93,7 @@ pub static WINDBG_L0: WinDbgLogger = WinDbgLogger {
     _priv: (),
 };
 
-#[cfg(feature = "simple_shared")]
+#[cfg(all(target_os="windows",feature="gui"))]
 pub fn windbg_simple_combo(log_lvl: LevelFilter) -> Box<dyn simplelog::SharedLogger> {
     match log_lvl {
         LevelFilter::Error => Box::new(WINDBG_L1),
@@ -104,7 +104,7 @@ pub fn windbg_simple_combo(log_lvl: LevelFilter) -> Box<dyn simplelog::SharedLog
         LevelFilter::Off => Box::new(WINDBG_L0),
     }
 }
-#[cfg(feature = "simple_shared")]
+#[cfg(all(target_os="windows",feature="gui"))]
 impl simplelog::SharedLogger for WinDbgLogger {
     // allows using with simplelog's CombinedLogger
     fn level(&self) -> LevelFilter {
