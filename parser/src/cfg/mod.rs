447225609c99672a3b4a83648fd7760a7c84df6b
eugenesvk <eugenesvk@users.noreply.github.com>
2024-05-28 03:05:09 +0700

    clippy

--- parser/src/cfg/mod.rs
+++ parser/src/cfg/mod.rs
@@ -2311,13 +2311,13 @@ fn parse_push_message(ac_params: &[SExpr], s: &ParserState) -> Result<&'static K
 pub mod windows;
 #[cfg(any(target_os = "windows", target_os = "unknown"))]
 pub use windows::*;
-#[cfg(any(target_os = "linux"))]
+#[cfg(target_os = "linux")]
 pub mod linux;
-#[cfg(any(target_os = "linux"))]
+#[cfg(target_os = "linux")]
 pub use linux::*;
-#[cfg(any(target_os = "macos"))]
+#[cfg(target_os = "macos")]
 pub mod macos;
-#[cfg(any(target_os = "macos"))]
+#[cfg(target_os = "macos")]
 pub use macos::*;
 
 fn to_simple_expr(params: &[SExpr], s: &ParserState) -> Vec<SimpleSExpr> {
