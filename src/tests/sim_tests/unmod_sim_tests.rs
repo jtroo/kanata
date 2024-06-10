dac4df70be396ee6255826ed99031878ba95894d
jtroo <j.andreitabs@gmail.com>
2024-06-09 17:23:47 -0700

    wip docs

--- src/tests/sim_tests/unmod_sim_tests.rs
+++ src/tests/sim_tests/unmod_sim_tests.rs
@@ -1,9 +1,9 @@
 use super::*;
 
 #[test]
-fn special_nop_keys() {
+fn unmod_keys_functionality_works() {
     let result = simulate(
-        r##"
+        "
          (defcfg)
          (defsrc f1 1 2 3 4 5 6 7 8 9 0)
          (deflayer base
@@ -19,7 +19,7 @@ fn special_nop_keys() {
              (unmod (ralt) i)
              (unmod (lctl lsft lmet lalt) j)
          )
-        "##,
+        ",
         "d:f1 t:5 d:1 u:1 t:5 d:2 u:2 t:5 d:3 u:3 t:5 d:4 u:4 t:5 d:5 u:5 t:5 d:6 u:6 t:5
                   d:7 u:7 t:5 d:8 u:8 t:5 d:9 u:9 t:5 d:0 u:0 t:5",
     )
@@ -39,5 +39,44 @@ fn special_nop_keys() {
          up:RGui dn:I up:I dn:RGui \
          up:LCtrl up:LShift up:LGui up:LAlt dn:J up:J dn:LCtrl dn:LShift dn:LGui dn:LAlt",
         result
+    );
+}
+
+#[test]
+#[should_panic]
+fn unmod_keys_mod_list_cannot_be_empty() {
+    simulate(
+        "
+         (defcfg)
+         (defsrc a)
+         (deflayer base (unmod () a))
+        ",
+        "",
+    );
+}
+
+#[test]
+#[should_panic]
+fn unmod_keys_mod_list_cannot_have_nonmod_key() {
+    simulate(
+        "
+         (defcfg)
+         (defsrc a)
+         (deflayer base (unmod (lmet c) a))
+        ",
+        "",
+    );
+}
+
+#[test]
+#[should_panic]
+fn unmod_keys_mod_list_cannot_have_empty_keys_after_mod_list() {
+    simulate(
+        "
+         (defcfg)
+         (defsrc a)
+         (deflayer base (unmod (lmet)))
+        ",
+        "",
     );
 }
