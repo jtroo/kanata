899af95f764d6130ba43db7e9417552b7f4ae3ae
jtroo <j.andreitabs@gmail.com>
2024-06-16 01:43:36 -0700

    wip

--- src/kanata/output_logic/zipchord.rs
+++ src/kanata/output_logic/zipchord.rs
@@ -1,9 +1,5 @@
 use kanata_parser::trie::Trie;
 
-/// Tracks number of inputs used to form a chord.
-/// If a chord activates, this number of backspaces will be added.
-struct InputCount {}
-
 /// Tracks current input to check against possible chords.
 /// This does not store by the input order;
 /// instead it is by some consistent ordering for
@@ -24,7 +20,7 @@ struct PrioritizePossibleChords {
 
 /// All possible chords.
 struct PossibleChords {
-    chords: Trie<Box<str>>
+    chords: Trie<(Box<str>, Option<Trie<Box<str>>>)>
 }
 
 #[derive(Debug, Clone, Hash, PartialEq, Eq)]
@@ -37,8 +33,43 @@ struct SortedChord {
 impl SortedChord {
     fn insert(&mut self, key: u16) {
         match self.keys.binary_search(&key) {
-            Ok(pos) => {} // element already in vector @ `pos`
+            Ok(pos) => {} // Element already in vector @ `pos`. Normally this wouldn't be expected
+                          // to happen but it turns out that key repeat might get in the way of
+                          // this assumption.
             Err(pos) => self.keys.insert(pos, key),
         }
     }
+}
+
+enum ChordEnabledState {
+    Enabled,
+    Disabled,
+}
+
+struct DynamicState {
+    /// Input sorted not by input order but by some consistent ordering such that it can be used to
+    /// compare against a Trie.
+    sorted_inputs: SortedInputs,
+    /// Whether chording should be enabled or disabled.
+    /// Chording will be disabled if:
+    /// - further presses cannot possibly activate a chord
+    /// - a release happens with no chord having been activated
+    /// Once disabled, chording will be enabled when:
+    /// - all keys have been released
+    enabled_state: ChordEnabledState,
+    /// Is Some when a chord has been activated which has possible follow-up chords.
+    /// E.g. dy -> day
+    ///      dy 1 -> Monday
+    ///      dy 2 -> Tuesday
+    /// Using the example above, when dy has been activated, the `1` and `2` activations will be
+    /// contained within `prioritized_chords`. This is cleared if the input is such that an
+    /// activation is no longer possible.
+    prioritized_chords: Option<PrioritizePossibleChords>
+}
+
+struct State {
+    dynamic: DynamicState,
+    /// Chords configured by the user. This is fixed at runtime other than live-reloads replacing
+    /// the state.
+    chords: PossibleChords,
 }
