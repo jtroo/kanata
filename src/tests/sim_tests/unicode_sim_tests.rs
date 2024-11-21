dc1321d2270ebe2fe40879beffce3526e04ffb44
jtroo <j.andreitabs@gmail.com>
2024-11-20 23:11:02 -0800

    doc: refactor config.adoc (#1366)

--- src/tests/sim_tests/unicode_sim_tests.rs
+++ src/tests/sim_tests/unicode_sim_tests.rs
@@ -1,17 +1,18 @@
 use super::*;
 
 #[test]
-fn special_nop_keys() {
+fn unicode() {
     let result = simulate(
         r##"
          (defcfg)
-         (defsrc 6 7 8 9 0)
+         (defsrc 6 7 8 9 0 f1)
          (deflayer base
              (unicode r#"("#)
              (unicode r#")"#)
              (unicode r#"""#)
              (unicode "(")
              (unicode ")")
+             (tap-dance 200 (f1(unicode 😀)f2(unicode 🙂)))
          )
         "##,
         "d:6 d:7 d:8 d:9 d:0 t:100",
