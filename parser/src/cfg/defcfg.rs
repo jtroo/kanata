d870227f9955a2fd3e981c2af00ba156ad43a242
jtroo <j.andreitabs@gmail.com>
2025-07-11 21:21:21 -0700

    misc: fix clippy

--- parser/src/cfg/defcfg.rs
+++ parser/src/cfg/defcfg.rs
@@ -17,7 +17,7 @@ pub enum DeviceDetectMode {
 #[cfg(any(target_os = "linux", target_os = "unknown"))]
 impl std::fmt::Display for DeviceDetectMode {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:?}", self)
+        write!(f, "{self:?}")
     }
 }
 
