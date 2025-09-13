jtroo/kanata

commit 19eaba2b022956831a72a32f7bd7a6ac3196300f

fix(switch): make XX actions work with input (#1789)

keyberon/src/layout.rs
@@ -292,6 +292,9 @@ pub enum State<'a, T: 'a> {
     SeqCustomPending(&'a T),
     SeqCustomActive(&'a T),
     Tombstone,
+    NoOpInput {
+        coord: KCoord,
+    },
 }
 impl<T> Copy for State<'_, T> {}
 impl<T> Clone for State<'_, T> {
@@ -312,6 +315,7 @@ impl<'a, T: 'a> State<'a, T> {
             NormalKey { coord, .. }
             | LayerModifier { coord, .. }
             | Custom { coord, .. }
+            | NoOpInput { coord }
             | RepeatingSequence { coord, .. } => Some(*coord),
             _ => None,
         }
@@ -1501,6 +1505,11 @@ impl<'a, const C: usize, const R: usize, T: 'a + Copy + std::fmt::Debug> Layout<
         if self.states.is_empty() || !matches!(current_custom, CustomEvent::NoEvent) {
             return current_custom;
         }
+        // It is important to note that this code cannot simply be replaced by `retain_mut`.
+        // The `retain_mut` function is not chosen
+        // because it is important to break on the first `SeqCustom` that is discovered.
+        // Such functionality could be replaced by a marker to ignore processing,
+        // but for now that is not necessary.
         self.states.retain(|s| !matches!(s, State::Tombstone));
         for state in self.states.iter_mut() {
             match state {
@@ -1682,6 +1691,7 @@ impl<'a, const C: usize, const R: usize, T: 'a + Copy + std::fmt::Debug> Layout<
                         .handle_press(OneShotHandlePressKey::Other(coord));
                 }
                 self.rpt_action = Some(action);
+                let _ = self.states.push(NoOpInput { coord });
             }
             Src => {
                 let action = &self.src_keys[usize::from(coord.1)];

src/tests/sim_tests/switch_sim_tests.rs
@@ -35,3 +35,19 @@ fn sim_switch_base_layer() {
     .no_time();
     assert_eq!("out:↓X out:↑X out:↓Y out:↑Y out:↓Y out:↑Y", result);
 }
+
+#[test]
+fn sim_switch_noop() {
+    let result = simulate(
+        "
+         (defsrc)
+         (deflayermap (-) a XX b (switch
+          ((input real a)) c break
+          () d break
+         ))
+        ",
+        "d:a t:10 d:b t:10",
+    )
+    .no_time();
+    assert_eq!("out:↓C", result);
+}

parser/src/cfg/mod.rs
@@ -1639,11 +1639,6 @@ fn parse_action_atom(ac_span: &Spanned<String>, s: &ParserState) -> Result<&'sta
             }
         }
         "XX" | "✗" | "∅" | "•" => {
-            if s.pctx.is_within_defvirtualkeys {
-                log::warn!(
-                    "XX within defvirtualkeys is likely incorrect. You should use nop0-nop9 instead."
-                );
-            }
             return Ok(s.a.sref(Action::NoOp));
         }
         "lrld" => return custom(CustomAction::LiveReload, &s.a),

commit e4582a907f0bdf60fbafba7039410185cb596f75

fix(noop): release XX action correctly (#1791)
Fix a regression introduced in:
19eaba2b022956831a72a32f7bd7a6ac3196300f.

keyberon/src/layout.rs
@@ -338,6 +338,7 @@ impl<'a, T: 'a> State<'a, T> {
             NormalKey { coord, .. }
             | LayerModifier { coord, .. }
             | RepeatingSequence { coord, .. }
+            | NoOpInput { coord }
                 if coord == c =>
             {
                 None

src/tests/sim_tests/switch_sim_tests.rs
@@ -46,8 +46,8 @@ fn sim_switch_noop() {
           () d break
          ))
         ",
-        "d:a t:10 d:b t:10",
+        "d:a d:b t:10 u:a u:b t:10 d:b u:b t:10",
     )
     .no_time();
-    assert_eq!("out:↓C", result);
+    assert_eq!("out:↓C out:↑C out:↓D out:↑D", result);
 }