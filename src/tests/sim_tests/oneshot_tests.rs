c4939e2e569f29368ef4e337573fc82a67c7961c
jtroo <j.andreitabs@gmail.com>
2025-12-05 23:45:04 -0800

    fix(oneshot): release other actions on same key

--- src/tests/sim_tests/oneshot_tests.rs
+++ src/tests/sim_tests/oneshot_tests.rs
@@ -41,3 +41,37 @@ fn oneshot_pause() {
     .to_ascii();
     assert_eq!("t:10ms dn:LAlt t:20ms dn:Kb1 t:5ms up:LAlt up:Kb1", result);
 }
+
+#[test]
+fn oneshot_multi_with_chord() {
+    let result = simulate(
+        "(defsrc a)
+         (deflayer test (multi a (one-shot 1000 C-z)))",
+        "d:KeyA t:10 u:KeyA t:200 d:KeyC t:1000",
+    )
+    .to_ascii();
+    assert_eq!(
+        "dn:A dn:LCtrl dn:Z t:10ms up:A t:200ms dn:C t:5ms up:LCtrl up:Z",
+        result
+    );
+}
+
+#[test]
+fn oneshot_multi_with_layer() {
+    let result = simulate(
+        "(defsrc a)
+         (deflayer l1 (multi a (one-shot 100 (layer-while-held l2))))
+         (deflayer l2 b)
+        ",
+        "d:KeyA t:10 u:KeyA t:10 d:KeyA t:10 u:KeyA t:1000",
+    )
+    .to_ascii();
+    // Known bug:
+    // The 5ms should be 10ms.
+    // The B is released (with instant action delay) even when it shouldn't
+    // because one-shot completion is releasing everything
+    // that is on the same coordinate as it.
+    //   issue: -------------------------------.
+    //                                         v
+    assert_eq!("dn:A t:10ms up:A t:10ms dn:B t:5ms up:B", result);
+}
