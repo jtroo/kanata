jtroo/kanata

commit 9c5ccb762fb37106305ca2f3702daa0f3267fcb1

feat(macOS): add support for IntlRo key (#1809)

parser/src/keys/mod.rs
@@ -323,12 +323,14 @@ pub fn str_to_oscode(s: &str) -> Option<OsCode> {
         #[cfg(any(target_os = "linux", target_os = "unknown"))]
         "katakana" => OsCode::KEY_KATAKANA,
         "cnv" | "conv" | "henk" | "hnk" | "henkan" => OsCode::KEY_HENKAN,
-        "ncnv" | "mhnk" | "muhenkan" => OsCode::KEY_MUHENKAN,
-        "IntlRo" | "ro" => OsCode::KEY_RO,
+        "ncnv" | "mhnk" | "muhenkan" => OsCode::KEY_MUHENKAN,       
         #[cfg(target_os = "macos")]
         "Lang1" | "kana" => OsCode::KEY_HANGEUL,
         #[cfg(any(target_os = "macos", target_os = "unknown"))]
         "Lang2" | "eisu" => OsCode::KEY_HANJA,
+
+        #[cfg(any(target_os = "windows", target_os = "macos"))]
+        "IntlRo" | "ro" => OsCode::KEY_RO,
 
         #[cfg(any(target_os = "linux", target_os = "unknown"))]
         "PrintScreen" | "prtsc" | "prnt" | "⎙" => OsCode::KEY_SYSRQ,

parser/src/keys/macos.rs
@@ -524,6 +524,10 @@ impl TryFrom<OsCode> for PageCode {
                 page: 0x07,
                 code: 0x85,
             }),
+            OsCode::KEY_RO => Ok(PageCode {
+                page: 0x07,
+                code: 0x87,
+            }),
             OsCode::KEY_HANGEUL => Ok(PageCode {
                 page: 0x07,
                 code: 0x90,
@@ -1194,6 +1198,10 @@ impl TryFrom<PageCode> for OsCode {
                 page: 0x07,
                 code: 0x85,
             } => Ok(OsCode::KEY_KPCOMMA),
+            PageCode {
+                page: 0x07,
+                code: 0x87,
+            } => Ok(OsCode::KEY_RO),
             PageCode {
                 page: 0x07,
                 code: 0x90,

commit bcf9042531c17524b33b04a85ab17ae8e2abc564

fix: add IntlRo and ro keys to wasm build
Fixes a regression introduced in 9c5ccb7

parser/src/keys/mod.rs
@@ -331,7 +331,7 @@ pub fn str_to_oscode(s: &str) -> Option<OsCode> {
         #[cfg(any(target_os = "macos", target_os = "unknown"))]
         "Lang2" | "eisu" => OsCode::KEY_HANJA,
 
-        #[cfg(any(target_os = "windows", target_os = "macos"))]
+        #[cfg(any(target_os = "windows", target_os = "macos", target_os = "unknown"))]
         "IntlRo" | "ro" => OsCode::KEY_RO,
 
         #[cfg(any(target_os = "linux", target_os = "android", target_os = "unknown"))]