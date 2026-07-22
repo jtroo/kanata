311c54a4cf7ba4ea6feed3673e20b38bac3ad1fc
jtroo <jtroo@roselily.ca>
2026-07-22 06:23:57 -0700

    fix: clippy (#2118)

--- parser/src/cfg/sexpr.rs
+++ parser/src/cfg/sexpr.rs
@@ -220,7 +220,7 @@ impl SExpr {
 impl std::fmt::Debug for SExpr {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            SExpr::Atom(a) => write!(f, "{}", &a.t),
+            SExpr::Atom(a) => write!(f, "{}", a.t),
             SExpr::List(l) => {
                 let inner =
                     l.t.iter()
